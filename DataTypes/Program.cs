4281c14580ca84d3929070f8936140104d36a171
user <user@DESKTOP-UVF6VSH>
2025-10-22 16:26:29 +0300

    .

---
+++ DataTypes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+	internal class Program
+	{
+		const string delimiter = "\n------------------------------------\n";
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"bool занимает {sizeof(bool)} Байт памяти, класс обвертка Boolean;");
+			Console.WriteLine(bool.FalseString);
+			Console.WriteLine(bool.TrueString);
+			Console.WriteLine(typeof(bool));
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine($@"тип данных char занимает {sizeof(char)} Байт памяти, и принимает значение в диапозоне от {(int)char.MinValue} до {(int)char.MaxValue}. 
+            Класс-обвертка - {typeof(char)}");
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine(@"RAW - строка игнорируется все специальные символы и escape - последовательности \t\n, 
+            т.е. воспринимается как есть");
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine($@"byte занимает {sizeof(byte)} Байт памяти,
+            и принимает значение в диапозоне от {byte.MinValue} до {byte.MaxValue}, 
+            класс -обвертка{typeof(byte)}");
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine($@"sbyte занимает {sizeof(sbyte)} Байт памяти,
+            и принимает значение в диапозоне от {sbyte.MinValue} до {sbyte.MaxValue}, 
+            класс-обвертка{typeof(sbyte)}");
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine($@"float занимает {sizeof(float) } байт: ");
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine($@"decimal занимает {sizeof(decimal)} байт: ");
+			Console.WriteLine(args);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine(5.0.GetType());
+			Console.WriteLine(delimiter);
+
+
+
+		}
+	}
+}
---
+++ Game/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+	class Program
+	{
+		static int x = 10, y = 10; //изначальная позиция
+		const char ICON = '@';//Барбос
+
+		static void Main()
+		{
+			Console.CursorVisible = false;//вырубаем мигающий курсор
+
+			while (true)
+			{
+				DrawIcon();//отрисовываем иконку
+				var key = Console.ReadKey(true).Key;//чтение нажатой клавиши из консоли
+				ClearIcon();//удаляем иконку
+
+				switch (key)
+				{
+					case ConsoleKey.UpArrow:
+					case ConsoleKey.W:
+						if (y > 0) y--;
+						break;
+					case ConsoleKey.DownArrow:
+					case ConsoleKey.S:
+						if (y < Console.WindowHeight - 1) y++;
+						break;
+					case ConsoleKey.LeftArrow:
+					case ConsoleKey.A:
+						if (x > 0) x--;
+						break;
+					case ConsoleKey.RightArrow:
+					case ConsoleKey.D:
+						if (x < Console.WindowWidth - 1) x++;
+						break;
+					case ConsoleKey.Escape:
+						return;
+				}
+			}
+		}
+
+		static void DrawIcon()
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(ICON);
+		}
+
+		static void ClearIcon()
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(' ');
+		}
+	}
+}
